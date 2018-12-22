4bf968877c80e2942bc414a94e45407eb4559745
Chris Ro <cro@nextgen.com>
2018-12-21 22:12:42 -0800

    0007 num ways to decode solution WIP

--- src/coding-problems/0007-num-ways-to-decode.ts
+++ src/coding-problems/0007-num-ways-to-decode.ts
@@ -12,37 +12,87 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 // time complexity: O(n^2)
 // space complexity: counter. O(1)
 
-const decodeMessage = (encodedMessage: string, counter: number = 0) => {
+const mapping = new Map();
+mapping.set(1, 'a');
+mapping.set(2, 'b');
+mapping.set(3, 'c');
+mapping.set(4, 'd');
+mapping.set(5, 'e');
+mapping.set(6, 'f');
+mapping.set(7, 'g');
+mapping.set(8, 'h');
+mapping.set(9, 'i');
+mapping.set(10, 'j');
+mapping.set(11, 'k');
+mapping.set(12, 'l');
+mapping.set(13, 'n');
+mapping.set(14, 'm');
+mapping.set(15, 'o');
+mapping.set(16, 'p');
+mapping.set(17, 'q');
+mapping.set(18, 'r');
+mapping.set(19, 's');
+mapping.set(20, 't');
+mapping.set(21, 'u');
+mapping.set(22, 'v');
+mapping.set(23, 'w');
+mapping.set(24, 'x');
+mapping.set(25, 'y');
+mapping.set(26, 'z');
+
+// console.log(mapping);
+
+const numDecodedMessages = [];
+
+const decodeMessage = (encodedMessage: string, decodedMsg: string = '') => {
     if (encodedMessage.length > 0) {
         const singleDigit: number = parseInt(encodedMessage[0]);
 
         console.log(`singleDigit = ${singleDigit}`);
 
         if (singleDigit > 0) {
-            counter += 1;
-            decodeMessage(encodedMessage.substring(1), counter);
+            // decodedMsg += mapping.get(singleDigit);
+            console.log(`decodedMsg SINGLE = ${decodedMsg}`);
+
+        } else {
+            console.log(`decodedMsg SINGLE FINAL = ${decodedMsg}`);
+            numDecodedMessages.push(decodedMsg);
+            // decodedMsg = `${singleDigit}`;
+            decodedMsg = mapping.get(singleDigit);
         }
+
+        decodeMessage(encodedMessage.substring(1), decodedMsg + mapping.get(singleDigit));
+
+    } else {
+        console.log(`decodedMsg SINGLE FINAL 2 = ${decodedMsg}`);
+        numDecodedMessages.push(decodedMsg);
     }
     
     if (encodedMessage.length > 1) {
         const doubleDigit: number = parseInt(encodedMessage.substring(0, 2));
    
         console.log(`doubleDigit = ${doubleDigit}`);
-
    
         if (doubleDigit >= 10 && doubleDigit <= 26) {
-            counter += 1;
-            decodeMessage(encodedMessage.substring(2), counter);
-        }
-    }
+            // decodedMsg += mapping.get(doubleDigit);
+            console.log(`counter DOUBLE = ${decodedMsg}`);
 
-    return counter;
+        } else {
+            console.log(`counter DOUBLE FINAL = ${decodedMsg}`);
+            numDecodedMessages.push(decodedMsg);
+            // decodedMsg = `${doubleDigit}`;
+            decodedMsg = mapping.get(doubleDigit);
+        }
+
+        decodeMessage(encodedMessage.substring(2), decodedMsg + mapping.get(doubleDigit));
+    }
 };
 
-const input: string = `111`;
+const input: string = `11125234`;
 
-console.log(decodeMessage(input));
+decodeMessage(input);
 
-
+console.log(`numDecodedMessages.length = ${numDecodedMessages.length}`);
+console.log(numDecodedMessages);
 
 export {};
