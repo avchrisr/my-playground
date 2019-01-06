0ec907321dc322d140ac1e876fc29b0fbe754bd1
Chris Ro <cro@nextgen.com>
2019-01-06 13:38:57 -0800

    added bubble sort implementation

---
+++ src/sorting-algorithms/bubble-sort.ts
@@ -0,0 +1,31 @@
+// BUBBLE SORT  --  O(n^2) time | O(1) space
+
+const bubbleSort = (input: number[]): number[] => {
+    for (let i = input.length; i > 0; i--) {
+        let swapped = false;
+        
+        for (let j=0; j < i - 1; j++) {
+            if (input[j] > input[j+1]) {
+                // swap
+                const temp = input[j];
+                input[j] = input[j+1];
+                input[j+1] = temp;
+
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            // array is already sorted. no need further iteration. break
+            i = 0;
+        }
+    }
+    return input;
+};
+
+const input = [1,98,25,34,3,2,98,55,35,31];
+
+console.log(bubbleSort(input));
+
+
+export {};
