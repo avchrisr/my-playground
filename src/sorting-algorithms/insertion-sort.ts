7795a99887b678fc7b2c4ded5ab99c9594b27bf7
Chris Ro <cro@nextgen.com>
2019-01-14 22:47:48 -0800

    updated insertion-sort with my own implementation

--- src/sorting-algorithms/insertion-sort.ts
+++ src/sorting-algorithms/insertion-sort.ts
@@ -1,18 +1,26 @@
 // INSERTION SORT  --  O(n^2) time | O(1) space
 
-const insertionSort = (input: number[]): number[] => {
-    for (let i=1; i < input.length; i++) {
-        let currVal = input[i];
-        let j = i-1;
+const insertionSort = (arr: number[]): number[] => {
 
-        while (j >= 0 && currVal < input[j]) {
-            input[j+1] = input[j];
-            j -= 1;
+    // two pointers. x and y
+    // x starts at index 1 and increments till the end of the array
+    // y = x-1 and decrements until conditions are met
+    //    while (y >= 0 && arr[y+1] < arr[y])
+    //        swap
+    //        y--
+
+    for (let x=1; x < arr.length; x++) {
+        let y = x-1;
+        while (y >= 0 && arr[y+1] < arr[y]) {
+            // swap
+            const temp = arr[y+1];
+            arr[y+1] = arr[y];
+            arr[y] = temp;
+            y -= 1;
         }
-        
-        input[j+1] = currVal;
     }
-    return input;
+        
+    return arr;
 };
 
 const input = [1,98,25,34,3,2,98,55,35,31];
